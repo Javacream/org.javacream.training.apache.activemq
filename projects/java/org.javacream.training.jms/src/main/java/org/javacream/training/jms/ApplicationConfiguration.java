876a16564f70ff4cb56e20f97c5697ec13c620bb
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-12-15 15:22:37 +0100

    add Artemis Configuration

---
+++ projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/ApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package org.javacream.training.jms;
+
+public interface ApplicationConfiguration {
+	String brokerURL = "tcp://localhost:61616";
+	String queueName = "exampleQueue";
+	String topicName = "exampleTopic";
+	String username = "artemis";
+	String password = "simetraehcapa";
+
+
+}
