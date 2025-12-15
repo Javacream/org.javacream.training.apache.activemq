c55a67b2328a040f08ac3c6035dff6c61a910324
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-12-15 15:53:23 +0100

    finish producer

--- projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/basic/SimpleJmsProducer.java
+++ projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/basic/SimpleJmsProducer.java
@@ -1,5 +1,6 @@
 package org.javacream.training.jms.basic;
 
+import javax.jms.Queue;
 import javax.jms.Session;
 import static org.javacream.training.jms.ApplicationConfiguration.*;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
@@ -12,5 +13,22 @@ public class SimpleJmsProducer {
 		var session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		return session;
 	}
+	
+	public Queue getQueue(Session session) throws Exception {
+		return session.createQueue(queueName);
+	}
+	
+	public void sendMessage(Session session, String text) throws Exception {
+		var message = session.createTextMessage(text);
+		var messageProducer = session.createProducer(getQueue(session));
+		messageProducer.send(message);
+	}
+	
+	public static void main(String[] args) throws Exception{
+		var simpleJmsProducer = new SimpleJmsProducer();
+		var session = simpleJmsProducer.createSession();
+		simpleJmsProducer.sendMessage(session, "Hugo");
+	}
+	
 
 }
