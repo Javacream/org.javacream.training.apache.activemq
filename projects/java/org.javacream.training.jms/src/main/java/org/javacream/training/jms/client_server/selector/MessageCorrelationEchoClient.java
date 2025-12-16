320f2d7a35ab75233c941e97a4857edb1dc67c3e
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-12-16 13:41:55 +0100

    request reply with client-server

---
+++ projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/client_server/selector/MessageCorrelationEchoClient.java
@@ -0,0 +1,45 @@
+package org.javacream.training.jms.client_server.selector;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+
+import org.javacream.training.jms.util.JmsUtil;
+
+public class MessageCorrelationEchoClient {
+
+	public static void main(String[] args) throws Exception {
+		new MessageCorrelationEchoClient().testJms();
+	}
+	public void testJms() throws Exception {
+		
+		String id = "Client" + Math.abs(Math.random());
+		
+		ConnectionFactory connectionFactory = JmsUtil.getConnectionFactory();
+		
+		Connection connection = connectionFactory.createConnection();
+
+		Session session = connection.createSession(false,
+				Session.AUTO_ACKNOWLEDGE);
+		Destination requestDestination = session.createQueue(MessageCorrelationEchoConstants.REQUEST_DESTINATION);
+
+		Destination responseDestination = session.createQueue(MessageCorrelationEchoConstants.RESPONSE_DESTINATION);
+		Message request = session.createMessage();
+		request.setJMSCorrelationID(id);
+		request.setStringProperty(MessageCorrelationEchoConstants.PARAM_KEY, "Data from client " + id);
+
+		MessageProducer messageProducer = session.createProducer(requestDestination);
+		messageProducer.send(request);
+		messageProducer.close();
+		connection.start();
+		MessageConsumer consumer = session.createConsumer(responseDestination, "JMSCorrelationID='" + id + "'");
+		System.out.println(consumer.receive().getStringProperty(MessageCorrelationEchoConstants.RESULT_KEY));
+		consumer.close();
+		connection.close();
+	}
+
+}
---
+++ projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/client_server/selector/MessageCorrelationEchoServer.java
@@ -0,0 +1,61 @@
+package org.javacream.training.jms.client_server.selector;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+
+import org.javacream.training.jms.util.JmsUtil;
+
+public class MessageCorrelationEchoServer {
+
+	private static Session session;
+
+	public static void main(String[] args) throws Exception {
+		ConnectionFactory connectionFactory = JmsUtil.getConnectionFactory();
+		Connection connection = connectionFactory.createConnection();
+
+		session = connection.createSession(false,
+				Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue(MessageCorrelationEchoConstants.REQUEST_DESTINATION);
+
+
+		connection.start();
+		MessageConsumer consumer = session.createConsumer(destination);
+		consumer.setMessageListener(new DemoMessageListener());
+		Object sync = new Object();
+		synchronized (sync) {
+			sync.wait();
+		}
+	}
+
+	private static class DemoMessageListener implements MessageListener {
+
+		@Override
+		public void onMessage(Message receivedMessage) {
+			try {
+				String data = receivedMessage.getStringProperty(MessageCorrelationEchoConstants.PARAM_KEY);
+				System.out.println("Received message="
+						+ data + ", correlation=" + receivedMessage.getJMSCorrelationID());
+				Destination responseDestination = session.createQueue(MessageCorrelationEchoConstants.RESPONSE_DESTINATION);
+				MessageProducer producer = session.createProducer(responseDestination);
+				
+				Message response = session.createMessage();
+				
+				response.setStringProperty(MessageCorrelationEchoConstants.RESULT_KEY, "OK, echoing data " + data);
+				response.setJMSCorrelationID(receivedMessage.getJMSCorrelationID());
+				producer.send(response);
+				producer.close();
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+			
+		}
+
+	}
+}
