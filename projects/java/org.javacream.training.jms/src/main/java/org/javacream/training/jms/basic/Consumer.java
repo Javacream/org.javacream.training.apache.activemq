a02fe2322e2cd563812fd32b50b3ad5346108284
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2026-02-04 13:00:39 +0100

    add Producer and Consumer

---
+++ projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/basic/Consumer.java
@@ -0,0 +1,25 @@
+package org.javacream.training.jms.basic;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.javacream.training.jms.BrokerConfiguration;
+
+
+public class Consumer {
+
+	public static void main(String[] args) throws Exception{
+		var connectionFactory = new ActiveMQConnectionFactory(BrokerConfiguration.brokerUrl);
+		var connection = connectionFactory.createConnection(BrokerConfiguration.username, BrokerConfiguration.password);
+		var session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		var queue = session.createQueue(BrokerConfiguration.queueName);
+		var consumer = session.createConsumer(queue);
+		connection.start();
+		var message = consumer.receive();
+		var textMessage = (TextMessage)message;
+		System.out.println(textMessage.getText());
+		connection.close();
+		connectionFactory.close();
+	}
+
+}
