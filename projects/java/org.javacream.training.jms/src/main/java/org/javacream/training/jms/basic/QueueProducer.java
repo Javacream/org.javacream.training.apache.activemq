f6cd21ed624104c3b19ff8f5bdb6c63c059b568f
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2026-03-26 07:38:19 +0100

    queue and topic basics

--- projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/basic/QueueProducer.java
+++ projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/basic/QueueProducer.java
@@ -5,15 +5,15 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.javacream.training.jms.BrokerConfiguration;
 
 
-public class Producer {
+public class QueueProducer {
 
 	public static void main(String[] args) throws Exception{
 		var connectionFactory = new ActiveMQConnectionFactory(BrokerConfiguration.brokerUrl);
 		var connection = connectionFactory.createConnection(BrokerConfiguration.username, BrokerConfiguration.password);
 		var session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		var queue = session.createQueue(BrokerConfiguration.queueName);
+		var destination = session.createQueue(BrokerConfiguration.queueName);
 		var message = session.createTextMessage("Hello");
-		var messageProducer = session.createProducer(queue);
+		var messageProducer = session.createProducer(destination);
 		messageProducer.send(message);
 		connection.close();
 		connectionFactory.close();
--- projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/basic/TopicConsumer.java
+++ projects/java/org.javacream.training.jms/src/main/java/org/javacream/training/jms/basic/TopicConsumer.java
@@ -5,15 +5,14 @@ import javax.jms.TextMessage;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.javacream.training.jms.BrokerConfiguration;
 
-
-public class Consumer {
+public class TopicConsumer {
 
 	public static void main(String[] args) throws Exception{
 		var connectionFactory = new ActiveMQConnectionFactory(BrokerConfiguration.brokerUrl);
 		var connection = connectionFactory.createConnection(BrokerConfiguration.username, BrokerConfiguration.password);
 		var session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		var queue = session.createQueue(BrokerConfiguration.queueName);
-		var consumer = session.createConsumer(queue);
+		var destination = session.createTopic(BrokerConfiguration.topicName);
+		var consumer = session.createConsumer(destination);
 		connection.start();
 		var message = consumer.receive();
 		var textMessage = (TextMessage)message;
